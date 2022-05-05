8fffcf44f811b6884d23c66f681c3d6646af4b71
Goultard59 <goultard59@gmail.com>
2022-05-05 14:44:12 +0200

    Update createPPI_OmniPath.R

--- R/createPPI_OmniPath.R
+++ R/createPPI_OmniPath.R
@@ -14,26 +14,23 @@
 #' (character vector) with the genes not used in the interactions.
 #'
 #' @export
-createPPI <- function(filename, expr){
+createPPI <- function(expr, species){
   a <- expr
-  data = read.csv(filename, sep='\t', header=FALSE)
-  cat(dim(data))
+  if (species = "Hs") {
+    organism <- 9606
+  }
+  else if (species = "Mm") {
+    organism <- 10090
+  }
+  else if (species = "Rn") {
+    organism <- 10116
+  }
+  data <- as.data.frame(import_omnipath_interactions(datasets = 'omnipath', entity_types = 'protein', organism = organism))
+  cat(dim(ppi))
   cat('\nProtein interaction data loaded!')
   # The columns names are usually fixed.
-  ppi <- data[, c('V5','V6', 'V15')] #geneA and geneB, score
-  ppi <- as.data.frame(apply(ppi, 2, function(x)gsub('\\s+', '',x)))
-  geneA <- as.vector(unlist(lapply(strsplit(as.character(ppi[,'V5']),
-                                            split="\\:"), "[", 2)))
-  geneB <- as.vector(unlist(lapply(strsplit(as.character(ppi[,'V6']),
-                                            split="\\:"), "[", 2)))
-  score <- as.vector(unlist(lapply(strsplit(as.character(ppi[,'V15']),
-                                            split="\\:"), "[", 2)))
-  score <- as.numeric(score)
-  geneA <- as.vector(unlist(lapply(strsplit(as.character(geneA),
-                                            split="\\|"), "[", 1)))
-  geneB <- as.vector(unlist(lapply(strsplit(as.character(geneB),
-                                            split="\\|"), "[", 1)))
-  idmap <- data.frame(idA=geneA, idB=geneB, score=score)
+  ppi <- data[, c('source_genesymbol','target_genesymbol', 'curation_effort')] #geneA and geneB, score
+  idmap <- data.frame(idA=ppi$source_genesymbol, idB=ppi$target_genesymbol, score=as.numeric(ppi$curation_effort))
   allgenes <- unique(c(as.vector(idmap$idA), as.vector(idmap$idB)))
   inetwork <- igraph::graph.data.frame(idmap, directed=FALSE);
   inetwork <- igraph::simplify(inetwork, remove.multiple = TRUE,
