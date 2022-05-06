6fbc81274b7898f2cb092591491c31a4bec2f4f6
Goultard59 <goultard59@gmail.com>
2022-05-06 10:48:43 +0200

    Update Omnipath_LR_network.R

--- R/Omnipath_LR_network.R
+++ R/Omnipath_LR_network.R
@@ -1,21 +1,10 @@
-#' Create protein-protein interactions.
+#' Builds ligand-receptor network prior knowledge for fusca using Omnipath.
 #'
-#' Create a protein-protein interaction network for the reconstruction of cell
-#' signaling pathways.
-#'
-#' @param filename character; protein interaction data.
-#' @param expr dgCMatrix; gene expression in each cell, found in
-#' CellRouter@@ndata.
-#'
-#' @return list; the network (igraph object), the ctable (data frame) with the
-#' edge list, the table (data frame) with the information of the ids of gene A
-#' and B and the scores, the genes in total (character vector), genesPPI
-#' (character vector) with the genes in the interactions, and the remove
-#' (character vector) with the genes not used in the interactions.
+#' @return data frame containing ligand and receptor.
 #'
 #' @export
-Omnipath_LR_network <- function(species){
-  pairs <- nichenet_lr_network(only_omnipath = TRUE)
+Omnipath_LR_network <- function(){
+  pairs <- nichenet_lr_network(ramilowski = NULL)
   pairs$Pair.Name <- paste(pairs$from, pairs$to, sep = "_")
   return(pairs)
 }
